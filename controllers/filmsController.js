c222a2051044fd502740c6e914aeff54dd444ca9
Stefan Lyew <stefan.lyew@gmail.com>
2017-10-01 16:31:43 -0700

    refactor filmsController syntax

--- controllers/filmsController.js
+++ controllers/filmsController.js
@@ -2,18 +2,19 @@ const swapi = require('swapi-node');
 const filmsService = require('../services/filmsService');
 
 // TODO error handling 
-exports.index = function(req, res) {
-  swapi.getFilm().then((result) => {
-    const filteredResults = filmsService.filterFilmAttributes(result);
-    const groupedResults = filmsService.groupFilmsBy(
-      req.query.group_by,
-      filteredResults
-    );
+module.exports = {
+  index: (req, res) => {
+    swapi.getFilm().then((result) => {
+      const filteredResults = filmsService.filterFilmAttributes(result);
+      const groupedResults = filmsService.groupFilmsBy(
+        req.query.group_by,
+        filteredResults
+      );
 
-    res.send(groupedResults);
-  });
-};
-
-exports.characters = function(req, res) {
-    res.send('NOT IMPLEMENTED: List Characters for the film with id: ' + req.params.filmSwapiId);
-};
+      res.send(groupedResults);
+    });
+  },
+  characters: (req, res) => {
+      res.send('NOT IMPLEMENTED: List Characters for the film with id: ' + req.params.filmSwapiId);
+  }
+}
