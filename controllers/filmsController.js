af0e4e037c74f6f79d62d96bdfb2ff4c4c25c868
Stefan Lyew <stefan.lyew@gmail.com>
2017-10-01 15:00:24 -0700

    return films from swapi-api

--- controllers/filmsController.js
+++ controllers/filmsController.js
@@ -1,5 +1,12 @@
+const swapi = require('swapi-node');
+
+// TODO filter out only the data we need 
+// TODO groupBy directors
+// TODO error handling 
 exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: List Films, group by Director');
+  swapi.getFilm().then((result) => {
+    res.send(result);
+  });
 };
 
 exports.characters = function(req, res) {
