c53fd7df1cf0b77312fefba75e628b07e459dfad
Stefan Lyew <stefan.lyew@gmail.com>
2017-10-01 18:26:06 -0700

    add error handling to films controller

--- controllers/filmsController.js
+++ controllers/filmsController.js
@@ -1,16 +1,19 @@
 const charactersService = require('../services/charactersService');
 const filmsService = require('../services/filmsService');
 
-// TODO error handling 
 module.exports = {
   index: (req, res) => {
     filmsService.listFilms(req.query.group_by).then((films) => {
       res.send(films);
+    }).catch((err) => {
+      res.sendStatus(err.error);
     });
   },
   characters: (req, res) => {
     charactersService.listCharacters(req.params.filmSwapiId).then((films) => {
       res.send(films);
+    }).catch((err) => {
+      res.sendStatus(err.error);
     });
   }
 }
